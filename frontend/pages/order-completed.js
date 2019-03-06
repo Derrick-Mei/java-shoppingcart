e4aac7a999ebb4a395659554f59cf5a2ee5bf149
Simon Davis <simon_freelance@hotmail.com>
2019-03-06 14:49:03 -0500

    Basic order completed page implemented

---
+++ frontend/pages/order-completed.js
@@ -0,0 +1,29 @@
+import Reward from "react-rewards";
+import { Button } from "antd";
+import { useRef, useEffect } from "react";
+import styled from "styled-components";
+const OrderCompletedPage = () => {
+  let rewardRef = useRef(null);
+  useEffect(() => {
+    rewardRef.current.rewardMe();
+  }, []);
+  return (
+    <OrderWrapper>
+      <Reward ref={ref => (rewardRef.current = ref)} type="confetti">
+        <h2>Thanks for your order!</h2>
+        <Button>Back to shopping</Button>
+      </Reward>
+    </OrderWrapper>
+  );
+};
+const OrderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  top: 30%;
+  overflow: hidden;
+`;
+
+export default OrderCompletedPage;
