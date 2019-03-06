ad09e07a3be69a1c9b8628343a0a91ed07491f23
Simon Davis <simon_freelance@hotmail.com>
2019-03-06 17:56:55 -0500

    Fixed signup controller

--- backend/src/main/java/com/lambdaschool/coffeebean/controller/Signupcontroller.java
+++ backend/src/main/java/com/lambdaschool/coffeebean/controller/Signupcontroller.java
@@ -13,31 +13,25 @@ import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping(value = "/signup", produces = MediaType.APPLICATION_JSON_VALUE)
-public class Signupcontroller
-{
+public class Signupcontroller {
 
     @Autowired
     // private UserService userService;
     private Userrepository userrepos;
 
     @PostMapping("")
-    public Object addNewUser(@RequestBody User newuser) throws URISyntaxException
-    {
-        if (userrepos.findByUsername(newuser.getUsername()) != null)
-        {
-            if (newuser.getEmail() != null & userrepos.findByEmail(newuser.getEmail()) != null)
-            {
-                return "{ username unique constraint : " + newuser.getUsername() + " already exists,\nemail unique constraint : " + newuser.getEmail() + " already exists }";
+    public Object addNewUser(@RequestBody User newuser) throws URISyntaxException {
+        if (userrepos.findByUsername(newuser.getUsername()) != null) {
+            if (newuser.getEmail() != null & userrepos.findByEmail(newuser.getEmail()) != null) {
+                return "{ username unique constraint : " + newuser.getUsername()
+                        + " already exists,\nemail unique constraint : " + newuser.getEmail() + " already exists }";
             }
             return "{username unique constraint : " + newuser.getUsername() + " already exists}";
-        }
-        else if (userrepos.findByEmail(newuser.getEmail()) != null)
-        {
+        } else if (userrepos.findByEmail(newuser.getEmail()) != null & newuser.getEmail() != null) {
             return "{ email unique constraint : " + newuser.getEmail() + " already exists }";
-        }
-        else
-        {
-            // set role to user for security concern.  Just in case a new user wants to set their own role to admin.
+        } else {
+            // set role to user for security concern. Just in case a new user wants to set
+            // their own role to admin.
             newuser.setRole("user");
             return userrepos.save(newuser);
         }
