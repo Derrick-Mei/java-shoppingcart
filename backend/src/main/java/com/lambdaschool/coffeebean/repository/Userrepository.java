393f1ccd16a0020e80b526d2631a5644ea1c4f1f
Derrick Mei <derrick.mei.168@gmail.com>
2019-03-07 17:06:28 -0600

    updated: Userrepository - added cart queries

--- backend/src/main/java/com/lambdaschool/coffeebean/repository/Userrepository.java
+++ backend/src/main/java/com/lambdaschool/coffeebean/repository/Userrepository.java
@@ -1,9 +1,12 @@
 package com.lambdaschool.coffeebean.repository;
 
+import com.lambdaschool.coffeebean.model.CartItems;
 import com.lambdaschool.coffeebean.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface Userrepository extends JpaRepository<User, Long>
@@ -15,6 +18,21 @@ public interface Userrepository extends JpaRepository<User, Long>
     @Query(value = "SELECT * FROM cart WHERE userid = :userid", nativeQuery = true)
     List<Object> getItemsInCartById(long userid);
 
-//    @Query(value = "", nativeQuery = true)
-//    List<CartItems> getItemsInCartById2(UUID userid);
+    @Query(value = "SELECT p.productname, p.description, p.image, p.price, c.quantityincart FROM cart c INNER JOIN products p ON c.productid=p.productid WHERE c.userid=:userid", nativeQuery = true)
+    List<CartItems> getCartItemsInCartById(long userid);
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO cart (userid, productid, quantityincart) VALUES (:userid, :productid, :quantity)", nativeQuery = true)
+    void postItemToCart(long userid, long productid, int quantity);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM cart WHERE userid=:userid AND productid=:productid", nativeQuery = true)
+    void deleteOneItemFromCart(long userid, long productid);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM cart WHERE userid=:userid", nativeQuery = true)
+    void deleteAllItemsFromCart(long userid);
 }
