cd1ea785bcbbeed50f752fa6971d560288e86ccc
Simon Davis <simonfreelance1@gmail.com>
2019-03-12 18:34:03 -0400

    Changed axios instance to choose its own scope where its declared

--- frontend/lib/axiosInstances.js
+++ frontend/lib/axiosInstances.js
@@ -3,13 +3,15 @@ import axios from "axios";
 const baseAxios = axios.create({
     baseURL: process.env.BACKEND_URL,
   });
+function createBearerAxios() {
+  return axios.create({
+    baseURL: `${process.env.BACKEND_URL}`,
+    headers: {
+      Authorization: "Bearer " + window.localStorage.getItem("access_token"),
+      "Content-Type": "application/json"
+    },
+  });
+}
 
-const bearerAxios = axios.create({
-  baseURL: `${process.env.BACKEND_URL}`,
-  headers: {
-    // Authorization: "Bearer " + window.localStorage.getItem("access_token"),
-    "Content-Type": "application/json"
-  }
-});
 
-export { baseAxios, bearerAxios };
+export { baseAxios, createBearerAxios };
