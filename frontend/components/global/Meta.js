2d0533f55a1e20db195c837b0f7e707abad0d31f
Simon Davis <simonfreelance1@gmail.com>
2019-03-16 15:55:09 -0400

    Removed useless meta tag

--- frontend/components/global/Meta.js
+++ frontend/components/global/Meta.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
     <meta charSet="utf-8" />
     <link rel="shortcut icon" href="/static/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="/static/style.css" />
