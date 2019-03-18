65d3a2e813d5aa3cd8def77e398f6bbbea4f4fa7
Simon Davis <simonfreelance1@gmail.com>
2019-03-17 23:18:05 -0400

    Fixed attribute in Meta

--- frontend/components/global/Meta.js
+++ frontend/components/global/Meta.js
@@ -5,7 +5,7 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <meta
-      http-equiv="Content-Security-Policy"
+      httpEquiv="Content-Security-Policy"
       content="upgrade-insecure-requests"
     />
 
