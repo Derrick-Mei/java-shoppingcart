194a7a6fdba4a721d5ebe83df3f0926e2292184b
Simon Davis <simonfreelance1@gmail.com>
2019-03-19 13:42:58 -0400

    Implemented stripe a different way using a modal

---
+++ frontend/components/StripeButton.tsx
@@ -0,0 +1,51 @@
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+export interface Props {
+  headerImg: string;
+  amount: number;
+}
+
+const StripeBtn: React.SFC<Props> = ({headerImg, amount}) => {
+  const publishableKey = "pk_test_fbmMQVyluBRGwx3QN5yo0Bi8";
+
+  const onToken = (token: any) => {
+    const body = {
+      amount: amount,
+      token: token,
+    };
+    console.log(token);
+    // axios
+    //     .post("http://localhost:8000/payment", body)
+    //     .then(response => {
+    //       console.log(response);
+    //       alert("Payment Success");
+    //     })
+    //     .catch(error => {
+    //       console.log("Payment Error: ", error);
+    //       alert("Payment Error");
+    //     });
+    //TODO: Add a request to the server to finish payment
+    //Send a receipt to customer's email if they have one
+
+    //POST to server address information for next time
+    // Router.push({
+    //   pathname: "/order-completed",
+    // });
+  };
+  return (
+    <StripeCheckout
+      label="Add Credit Card" //Component button text
+      name="Mean Coffee Bean" //Modal Header
+      description="Finish your order."
+      panelLabel="Submit Order" //Submit button in modal
+      amount={amount} //Amount in cents $9.99
+      token={onToken}
+      stripeKey={publishableKey}
+      image={headerImg} //Pop-in header image
+      billingAddress={false}
+    />
+  );
+};
+
+export default StripeBtn;
