141f6a005ee688dd5d755d61255389ef95f43c0c
Simon Davis <simonfreelance1@gmail.com>
2019-03-23 17:14:40 -0400

    Fixed randomNumGenerator and added tests for it.

--- frontend/lib/randomGeneratorMethods.ts
+++ frontend/lib/randomGeneratorMethods.ts
@@ -1,11 +1,11 @@
-const generateRandomNumByRange = (min: number, max: number) => {
-  return Math.random() * (max - min) + min;
+const generateRandomNumByRange = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
-const chooseRandomItemInArray = (arr: [any]) => {
+function chooseRandomItemInArray<T>(arr: T[]): T {
   const randomIndex = Math.floor(Math.random() * arr.length);
   const randomItem = arr[randomIndex];
   return randomItem;
-};
+}
 
 export {generateRandomNumByRange, chooseRandomItemInArray};
