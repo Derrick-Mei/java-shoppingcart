ee243a7e7ea114b02920af2c21c04bd9596b483c
Simon Davis <simonfreelance1@gmail.com>
2019-03-27 13:25:21 -0400

    Able to get orderid when clicking on order

--- frontend/components/shop-components/OrderHistoryDrawer.tsx
+++ frontend/components/shop-components/OrderHistoryDrawer.tsx
@@ -30,21 +30,27 @@ const OrderHistoryDrawer: React.SFC<Props> = ({
         onClose={() => setIsMainVisible(false)}
       >
         <CardsList
-          onClick={() => {
+          onClick={e => {
             setIsChildVisible(true);
+            //TODO: Figure out event propogation with data attribute for better efficiency
+            console.log(e.target.dataset.orderid);
           }}
         >
           {Object.keys(ordersData).map((id: any) => {
             return (
-              <OrderCard
+              <OrderCardWrapper
+                data-orderid={id}
                 key={ordersData[id].keyId}
-                title={`Total Order Price: ${formatMoney(
-                  ordersData[id].totalPrice,
-                )}`}
               >
-                <p>Order Date: {ordersData[id].date}</p>
-                <p>Order Time: {ordersData[id].time}</p>
-              </OrderCard>
+                <OrderCard
+                  title={`Total Order Price: ${formatMoney(
+                    ordersData[id].totalPrice,
+                  )}`}
+                >
+                  <p>Order Date: {ordersData[id].date}</p>
+                  <p>Order Time: {ordersData[id].time}</p>
+                </OrderCard>
+              </OrderCardWrapper>
             );
           })}
         </CardsList>
@@ -65,7 +71,11 @@ const ParentDrawer = styled(Drawer)``;
 const ChildDrawer = styled(Drawer)``;
 
 const OrderCard = styled(Card)`
+  pointer-events: none;
+`;
+const OrderCardWrapper = styled.div`
   cursor: pointer;
+
   &:hover {
     -webkit-box-shadow: 0px 0px 5px 8px rgba(51, 51, 51, 0.06);
     -moz-box-shadow: 0px 0px 5px 8px rgba(51, 51, 51, 0.06);
