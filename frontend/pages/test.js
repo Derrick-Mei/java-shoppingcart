0a6e3f1d913ad34dbe6fc86bea9f891b15fdc5cc
Simon Davis <simonfreelance1@gmail.com>
2019-03-27 13:48:01 -0400

    Orders Drawer testing MVP completed

--- frontend/pages/test.js
+++ frontend/pages/test.js
@@ -3,41 +3,19 @@ import {useState, useEffect} from "react";
 import {Button} from "antd";
 import axios from "axios";
 import {createBearerAxios} from "../lib/axiosInstances";
-import orderDataManipulator from "../lib/manipulateData/orderDataManipulator";
-const mockOrderData = [
-  {
-    description: "description1",
-    orderid: 1,
-    productname: "brazil coffee",
-    price: 10.5,
-    image: "brazil-coffee-beans",
-    quantityinorder: 8,
-  },
-];
+import manipulateForOrderCards from "../lib/manipulateData/manipulateForOrderCards";
+import getCustomerOrdersByUserId from "../lib/requestsEndpoints/getCustomerOrdersByUserId";
+
 const TestPage = () => {
   const [isVisible, setIsVisible] = useState(true);
-  const [orders, setOrders] = useState([]);
-  useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        const {data} = await createBearerAxios({
-          method: "get",
-          url: `/orders/userid/${window.localStorage.getItem("userid")}`,
-        });
-        const ordersTable = orderDataManipulator(data);
-        setOrders(Object.values(ordersTable));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchOrders();
-  });
+  const [orders, setOrders] = useState({});
+  useEffect(() => {}, []);
   return (
     <>
       <OrderHistoryDrawer
         isMainVisible={isVisible}
         setIsMainVisible={setIsVisible}
-        mainData={mockOrderData}
+        ordersData={orders}
       />
       <Button onClick={() => setIsVisible(true)}>Open</Button>
     </>
@@ -57,4 +35,5 @@ onClick={() => {
 })}
 </CardsList> */
 }
+
 export default TestPage;
