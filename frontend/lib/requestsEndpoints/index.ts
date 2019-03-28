773f86bc955451aef03200c4e2ecab52f2ed6ccd
Simon Davis <simonfreelance1@gmail.com>
2019-03-28 12:49:40 -0400

    Fixed exporting problem for endpoints

--- frontend/lib/requestsEndpoints/index.ts
+++ frontend/lib/requestsEndpoints/index.ts
@@ -1,16 +1,18 @@
 // GET
-export * from "./getCartByUserId";
-export * from "./getCriteriaSearch";
-export * from "./getCustomerByUserId";
-export * from "./getCustomerByUsername";
-export * from "./getCustomerOrdersByUserId";
-export * from "./getOrderByOrderId";
-export * from "./getShopItemsByPage";
-export * from "./getShopMerchandise";
+export {default as getCartByUserId} from "./getCartByUserId";
+export {default as getCriteriaSearch} from "./getCriteriaSearch";
+export {default as getCustomerByUserId} from "./getCustomerByUserId";
+export {default as getCustomerByUsername} from "./getCustomerByUsername";
+export {
+  default as getCustomerOrdersByUserId,
+} from "./getCustomerOrdersByUserId";
+export {default as getOrderByOrderId} from "./getOrderByOrderId";
+export {default as getShopItemsByPage} from "./getShopItemsByPage";
+export {default as getShopMerchandise} from "./getShopMerchandise";
 // POST
-export * from "./postAddCartItem"
-export * from "./postCreateOrder";
+export {default as postAddCartItem} from "./postAddCartItem";
+export {default as postCreateOrder} from "./postCreateOrder";
 // PUT
-export * from "./putModifyCartQuantity";
+export {default as putModifyCartQuantity} from "./putModifyCartQuantity";
 // DELETE
-export * from "./deleteCartItemRequest";
+export {default as deleteCartItemRequest} from "./deleteCartItemRequest";
