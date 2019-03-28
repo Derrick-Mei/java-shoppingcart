630d056bbb37e90737f46404d7c1514235aea06b
Simon Davis <simonfreelance1@gmail.com>
2019-03-28 11:52:59 -0400

    Added a better way to import endpoints

---
+++ frontend/lib/requestsEndpoints/index.ts
@@ -0,0 +1,16 @@
+// GET
+export * from "./getCartByUserId";
+export * from "./getCriteriaSearch";
+export * from "./getCustomerByUserId";
+export * from "./getCustomerByUsername";
+export * from "./getCustomerOrdersByUserId";
+export * from "./getOrderByOrderId";
+export * from "./getShopItemsByPage";
+export * from "./getShopMerchandise";
+// POST
+export * from "./postAddCartItem"
+export * from "./postCreateOrder";
+// PUT
+export * from "./putModifyCartQuantity";
+// DELETE
+export * from "./deleteCartItemRequest";
